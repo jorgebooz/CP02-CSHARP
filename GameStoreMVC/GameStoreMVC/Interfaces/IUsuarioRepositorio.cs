f01d6fb95cac39b5bf99d7048fd25b2bf34fdcbf
Caio Hideki Cardenas Ishizu <caiohideki10@gmail.com>
2026-04-27 20:01:12 -0300

    feat(interfaces): definir contrato IUsuarioRepositorio com operações de usuário

--- GameStoreMVC/GameStoreMVC/Interfaces/IUsuarioRepositorio.cs
+++ GameStoreMVC/GameStoreMVC/Interfaces/IUsuarioRepositorio.cs
@@ -1,7 +1,11 @@
-﻿namespace GameStoreMVC.Interfaces
+﻿using GameStoreMVC.Models;
+
+namespace GameStoreMVC.Interfaces
 {
     public interface IUsuarioRepositorio
     {
-   
+        Usuario? BuscarPorEmail(string email);
+        void Cadastrar(Usuario usuario);
+        bool EmailJaCadastrado(string email);
     }
 }
