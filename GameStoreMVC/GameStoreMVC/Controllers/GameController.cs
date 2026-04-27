eedec3c1be6d01e2de37f2440be172263e34b9f2
CaioFreitasCF <caio.freitascfc@gmail.com>
2026-04-27 20:43:26 -0300

    feat(controllers): implementar GameController com CRUD de jogos

--- GameStoreMVC/GameStoreMVC/Controllers/GameController.cs
+++ GameStoreMVC/GameStoreMVC/Controllers/GameController.cs
@@ -1,9 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameStoreMVC.Interfaces;
+using GameStoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameStoreMVC.Controllers
 {
     public class GameController : Controller
     {
+        private readonly IGameRepositorio _gameRepositorio;
 
+        public GameController(IGameRepositorio gameRepositorio)
+        {
+            _gameRepositorio = gameRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            var games = _gameRepositorio.ListarTodos();
+            return View(games);
+        }
+
+        [HttpGet]
+        public IActionResult Criar() => View();
+
+        [HttpPost]
+        public IActionResult Criar(Game game)
+        {
+            if (!ModelState.IsValid) return View(game);
+            _gameRepositorio.Adicionar(game);
+            TempData["Sucesso"] = "Jogo cadastrado com sucesso!";
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var game = _gameRepositorio.BuscarPorId(id);
+            if (game == null) return NotFound();
+            return View(game);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Game game)
+        {
+            if (!ModelState.IsValid) return View(game);
+            _gameRepositorio.Atualizar(game);
+            TempData["Sucesso"] = "Jogo atualizado com sucesso!";
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Deletar(int id)
+        {
+            _gameRepositorio.Deletar(id);
+            TempData["Sucesso"] = "Jogo removido com sucesso!";
+            return RedirectToAction("Index");
+        }
     }
 }
--- GameStoreMVC/GameStoreMVC/Controllers/LoginController.cs
+++ GameStoreMVC/GameStoreMVC/Controllers/LoginController.cs
@@ -1,9 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameStoreMVC.Interfaces;
+using GameStoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameStoreMVC.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
 
+        public LoginController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _usuarioRepositorio.BuscarPorEmail(email);
+
+            if (usuario == null || usuario.Senha != senha)
+            {
+                ViewBag.Erro = "E-mail ou senha inválidos.";
+                return View();
+            }
+
+            HttpContext.Session.SetString("UsuarioNome", usuario.Nome);
+            HttpContext.Session.SetString("UsuarioEmail", usuario.Email);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult CriarConta() => View();
+
+        [HttpPost]
+        public IActionResult CriarConta(Usuario usuario)
+        {
+            if (!ModelState.IsValid) return View(usuario);
+
+            if (_usuarioRepositorio.EmailJaCadastrado(usuario.Email))
+            {
+                ModelState.AddModelError("Email", "Este e-mail já está cadastrado.");
+                return View(usuario);
+            }
+
+            _usuarioRepositorio.Cadastrar(usuario);
+            TempData["Sucesso"] = "Conta criada com sucesso! Faça login.";
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
     }
 }
