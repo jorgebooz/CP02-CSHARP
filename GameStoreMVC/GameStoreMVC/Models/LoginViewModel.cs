a70c4d5667adb1126ad2834779348e1459e5cded
Jorge Booz <107008455+jorgebooz@users.noreply.github.com>
2026-04-27 19:56:23 -0300

    feat(models): adicionar modelo Usuario com campos de cadastro e validação

--- GameStoreMVC/GameStoreMVC/Models/LoginViewModel.cs
+++ GameStoreMVC/GameStoreMVC/Models/LoginViewModel.cs
@@ -1,7 +1,27 @@
-﻿namespace GameStoreMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStoreMVC.Models
 {
-    
     public class Usuario
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A confirmação de senha é obrigatória")]
+        [Compare("Senha", ErrorMessage = "As senhas não coincidem")]
+        [DataType(DataType.Password)]
+        public string ConfirmarSenha { get; set; } = string.Empty;
     }
 }
--- GameStoreMVC/GameStoreMVC/obj/Release/net8.0/GameStoreMVC.AssemblyInfo.cs
+++ GameStoreMVC/GameStoreMVC/obj/Release/net8.0/GameStoreMVC.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStoreMVC")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e1c87a996c46837168d7b1c7928a18c82436e0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStoreMVC")]
